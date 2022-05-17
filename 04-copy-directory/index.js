5471629cfb11babb50435d91bf92f5e1ae482de5
Denis <denis6125038@gmail.com>
2022-05-17 21:21:25 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+function callback(err) {
+  if (err) throw err;
+  console.log('source.txt was copied to destination.txt');
+}
+
+function copyFiles(dirEntryList) {
+  dirEntryList.forEach((dirEntry) => {
+    const routeIn = path.join(__dirname, 'files', `${dirEntry.name}`);
+    const routeOut = path.join(__dirname, 'files-copy', `${dirEntry.name}`);
+    fs.copyFile(routeIn, routeOut, callback);
+  });
+}
+
+fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (error, dirEntryList) => {
+  try {
+
+    fs.access(path.join(__dirname, 'files-copy'), function(error){
+      if (error) {
+        fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+          if(err) throw err;
+          console.log('folder create successfully');
+          copyFiles(dirEntryList);
+        });
+      } else {
+        copyFiles(dirEntryList);
+      }
+    });
+
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
 
